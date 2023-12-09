ce19c47f079687909019371196d96b2c1a66d456
AkramCodes <sbntech72@gmail.com>
2023-12-09 15:25:32 +0530

    Add some codeLines

--- Portfolio.js
+++ Portfolio.js
@@ -5,3 +5,25 @@
 
     header.classList.toggle('sticky', window.scrollY > 100);
    };
+
+
+
+
+
+
+
+//    Swiper Design -----------------
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    grabCursor:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
